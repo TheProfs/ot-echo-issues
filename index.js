a43dd85ee9cafa59f591d731950f18c22e85ee84
nicholaswmin <nik.kyriakides@gmail.com>
2020-06-05 15:35:12 +0300

    improved readability

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 
 const util = require('util')
 const fs = require('fs')
-const json = require('./events/sample.json')
-
+const json = require('./sessions/1_MX40NjE4NjE2Mn5-MTU4NDM2MTgzNDk3OH5ScWZiTS9kV1ZtbGxIejk5ZkZyMUZ4Qy9-fg/events.json')
 const events = json.events
+const timestampToHumanReadable = timestamp => {
+  const date = new Date(timestamp)
+  const _date = date.toISOString().split('T')[0]
+  const _time = date.toISOString().split('T')[1].split('.')[0]
+
+  return _date + ' ' + _time
+}
 
 const publishers = events.filter(e => {
   return e.eventName === 'server_publisherCreated'
@@ -13,7 +19,8 @@ const publishers = events.filter(e => {
   return {
     user: e.processed.username,
     streamId: e.streamId,
-    timestamp: new Date(e.timestamp),
+    timestamp: e.timestamp,
+    date: timestampToHumanReadable(e.timestamp),
     subscribers: []
   }
 })
@@ -21,8 +28,7 @@ const publishers = events.filter(e => {
   return a.timestamp - b.timestamp
 })
 
-
-const subscribeEvents = events.filter(e => e.eventName === 'subscribe_success')
+events.filter(e => e.eventName === 'subscribe_success')
   .forEach(e => {
     const publisher = publishers.find(p => {
       return p.streamId === e.streamId
@@ -30,12 +36,23 @@ const subscribeEvents = events.filter(e => e.eventName === 'subscribe_success')
 
     publisher.subscribers.push({
       user: e.processed.username,
-      timestamp: new Date(e.timestamp)
+      date: timestampToHumanReadable(e.timestamp)
     })
   })
 
+events.filter(e => e.eventName === 'server_publisherDestroyed')
+  .forEach(e => {
+    const publisher = publishers.find(p => {
+      return p.streamId === e.streamId
+    })
+
+    publisher.destroyed = timestampToHumanReadable(e.timestamp)
+  })
+
 const split = publishers.reduce((acc, p) => {
-  const date = p.timestamp.toISOString().split('T')[0]
+  const date = timestampToHumanReadable(p.timestamp)
+
+  delete p.timestamp
 
   if (acc[date]) {
     acc[date].push(p)
